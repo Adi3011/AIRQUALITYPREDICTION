04c7d1d7edf20e6c3b57186c273aeef47c96e4b8
Chanchal Kumar Mishra <chanchalmishra1947@gmail.com>
2021-09-29 20:53:20 +0530

    graph

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ ann_model = pickle.load(open('mlModels/ann.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', prediction = 0)
 
 
 @app.route('/predict', methods=['POST'])
@@ -31,70 +31,7 @@ def predict():
         prediction = knn_model.predict(final_features)
     else:
         prediction = 'Something went Wrong!'
-    return prediction[0]
-
-# Routes
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-@app.route('/show', methods=['POST'])
-def show():
-    # fig = go.Figure(go.Indicator(
-    #     mode = "gauge+number+delta",
-    #     value = 420,
-    #     domain = {'x': [0, 1], 'y': [0, 1]},
-    #     title = {'text': "Speed", 'font': {'size': 24}},
-    #     delta = {'reference': 400, 'increasing': {'color': "RebeccaPurple"}},
-    #     gauge = {
-    #     'axis': {'range': [None, 500], 'tickwidth': 1, 'tickcolor': "darkblue"},
-    #     'bar': {'color': "darkblue"},
-    #     'bgcolor': "white",
-    #     'borderwidth': 2,
-    #     'bordercolor': "gray",
-    #     'steps': [
-    #         {'range': [0, 250], 'color': 'cyan'},
-    #         {'range': [250, 400], 'color': 'royalblue'}],
-    #     'threshold': {
-    #         'line': {'color': "red", 'width': 4},
-    #         'thickness': 0.75,
-    #         'value': 490}}))
-
-    # fig.update_layout(paper_bgcolor = "lavender", font = {'color': "darkblue", 'family': "Arial"})
-    # # fig.show()
-    # df = pd.DataFrame({
-    #   "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    #   "Amount": [4, 1, 2, 2, 4, 5],
-    #   "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-    # })
-   
-    # fig = px.bar(df, x="Fruit", y="Amount", color="City",    barmode="group")
-    # graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    # return render_template('index.html', graphJSON=graphJSON)
-    bar = create_plot()
-    return render_template('index.html', plot=bar)
-    # prediction = predict()
-    # return render_template('index.html', prediction='AQI: {:.2f}'.format(prediction), predicted=True)
-
-def create_plot():
-
-
-    N = 40
-    x = np.linspace(0, 1, N)
-    y = np.random.randn(N)
-    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
-
-
-    data = [
-        go.Bar(
-            x=df['x'], # assign x as the dataframe column 'x'
-            y=df['y']
-        )
-    ]
-
-    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('index.html', prediction=prediction[0], predicted=True)
 
 
 if __name__ == "__main__":
